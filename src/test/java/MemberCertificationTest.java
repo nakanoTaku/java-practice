d95a3fc80be5e2838dc394265f0a3bc2098224a2
tnakano <kosmo.waizu0804@gmail.com>
2018-02-20 15:46:13 +0900

    テストをリファクタリングした

--- src/test/java/MemberCertificationTest.java
+++ src/test/java/MemberCertificationTest.java
@@ -1,66 +1,172 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
 import java.text.ParseException;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
-import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MemberCertificationTest {
+@RunWith(Enclosed.class)
+public class MemberCertificationTest {
 
-    @org.junit.jupiter.api.Test
-    void main() {
+    private static MemberCertification certification;
 
+    @Before
+    public void setUp() {
+        certification = new MemberCertification();
     }
 
-    @org.junit.jupiter.api.Test
-    void executeAuthentication() throws ParseException {
-        Map<String, String> inputs = new LinkedHashMap<>();
-        inputs.put(MemberCertification.CertifiedItems.MAIL.getValue(), "oshima@gmail.com");
-        inputs.put(MemberCertification.CertifiedItems.BIRTH_DATE.getValue(), "1990/01/01");
-        inputs.put(MemberCertification.CertifiedItems.NAME.getValue(), "大嶋 一哉");
-        inputs.put(MemberCertification.CertifiedItems.TEL.getValue(), "090-1111-2222");
+    static class Fixture {
+        String value; // テストデータ
+        boolean expected; // 期待値
 
-        // 全部一致ならtrue
-        assertEquals (true, MemberCertification.executeAuthentication(inputs));
-        // 一つでも一致しなかったらfalse
-        inputs.put(MemberCertification.CertifiedItems.TEL.getValue(), "0120-33-33");
-        assertEquals (false, MemberCertification.executeAuthentication(inputs));
-    }
-
-    @org.junit.jupiter.api.Test
-    void checkEmail() {
-        assertEquals (true, MemberCertification.checkEmail("oshima@gmail.com"));
-        assertEquals (false, MemberCertification.checkEmail("hogehoge@gmail.com"));
-    }
-
-    @org.junit.jupiter.api.Test
-    void checkBirthday() throws ParseException {
-        assertEquals (true, MemberCertification.checkBirthday("1990/01/01"));
-        assertEquals (true, MemberCertification.checkBirthday("1990/01/1"));
-        assertEquals (true, MemberCertification.checkBirthday("1990/1/01"));
-        assertEquals (true, MemberCertification.checkBirthday("1990/1/1"));
-        assertEquals (false, MemberCertification.checkBirthday("1990/03/3"));
-        assertEquals (false, MemberCertification.checkBirthday("1990/3/3"));
-        try {
-            assertEquals (false, MemberCertification.checkBirthday("hoge"));
-        } catch (ParseException e) {
-            assertThat(e.getMessage(), equalTo("Unparseable date: \"hoge\""));
+        Fixture(String value, boolean expected) {
+            this.value = value;
+            this.expected = expected;
         }
     }
 
-    @org.junit.jupiter.api.Test
-    void checkName() {
-        assertEquals (true, MemberCertification.checkName("大嶋 一哉"));
-        assertEquals (false, MemberCertification.checkName("大嶋一哉"));
-        assertEquals (false, MemberCertification.checkName("中野 拓"));
+    @RunWith(Theories.class)
+    public static class アドレス_正しい場合 {
+        @DataPoint
+        public static Fixture FIXTURE =
+            new Fixture("oshima@gmail.com", true);
+
+
+        @Theory
+        public void checkEmail_名前が正しい場合はtrueが返却される(Fixture fixture) throws Exception {
+            boolean actual = certification.checkEmail(fixture.value);
+            boolean expected = fixture.expected;
+
+            assertThat(actual, is(expected));
+        }
     }
 
-    @org.junit.jupiter.api.Test
-    void checkTel() {
-        assertEquals (true, MemberCertification.checkTel("090-1111-2222"));
-        assertEquals (true, MemberCertification.checkTel("09011112222"));
-        assertEquals (false, MemberCertification.checkTel("0123-999-999"));
-        assertEquals (false, MemberCertification.checkTel("0123999999"));
+    @RunWith(Theories.class)
+    public static class アドレス_間違っている場合 {
+        @DataPoints
+        public static Fixture[] FIXTURES = {
+                new Fixture("hogehoge@hoge.jp", false),
+                new Fixture("", false)
+        };
+
+
+        @Theory
+        public void checkEmail_名前が間違っている場合はfalseが返却される(Fixture fixture) throws Exception {
+            boolean actual = certification.checkEmail(fixture.value);
+            boolean expected = fixture.expected;
+
+            assertThat(actual, is(expected));
+        }
+    }
+
+    @RunWith(Theories.class)
+    public static class 生年月日_正しい場合 {
+        @DataPoints
+        public static Fixture[] FIXTURES = {
+                new Fixture("1990/01/01", true),
+                new Fixture("1990/1/1", true)
+        };
+
+        @Theory
+        public void checkBirthday_生年月日が正しい場合はtrueが返却される(Fixture fixture) throws Exception {
+            boolean actual = certification.checkBirthday(fixture.value);
+            boolean expected = fixture.expected;
+
+            assertThat(actual,  is(expected));
+        }
+    }
+
+
+    @RunWith(Theories.class)
+    public static class 生年月日_型違い_例外発生 {
+        @DataPoints
+        public static Fixture[] FIXTURES = {
+                new Fixture("199011", false),
+                new Fixture("", false)
+        };
+
+        @Theory
+        @Test(expected = ParseException.class)
+        public void checkBirthday_生年月日がキャスト不能な場合は例外を送出する(Fixture fixture) throws Exception {
+            certification.checkBirthday(fixture.value);
+        }
+    }
+
+
+    @RunWith(Theories.class)
+    public static class 名前_正しい場合 {
+        @DataPoint
+        public static Fixture FIXTURE =
+                new Fixture("大嶋 一哉", true);
+
+
+        @Theory
+        public void checkName_名前が正しい場合はtrueが返却される(Fixture fixture) throws Exception {
+            boolean actual = certification.checkName(fixture.value);
+            boolean expected = fixture.expected;
+
+            assertThat(actual, is(expected));
+        }
+    }
+
+    @RunWith(Theories.class)
+    public static class 名前_間違っている場合 {
+        @DataPoints
+        public static Fixture[] FIXTURES = {
+                new Fixture("中野 拓", false),
+                new Fixture("", false)
+        };
+
+
+        @Theory
+        public void checkName_名前が間違っている場合はfalseが返却される(Fixture fixture) throws Exception {
+            boolean actual = certification.checkName(fixture.value);
+            boolean expected = fixture.expected;
+
+            assertThat(actual, is(expected));
+        }
+    }
+
+    @RunWith(Theories.class)
+    public static class 電話番号_正しい場合 {
+        @DataPoints
+        public static Fixture[] FIXTURE = {
+                new Fixture("090-1111-2222", true),
+                new Fixture("09011112222", true),
+        };
+
+
+        @Theory
+        public void checkTel_電話番号が正しい場合はtrueが返却される(Fixture fixture) throws Exception {
+            boolean actual = certification.checkTel(fixture.value);
+            boolean expected = fixture.expected;
+
+            assertThat(actual, is(expected));
+        }
+    }
+
+    @RunWith(Theories.class)
+    public static class 電話番号_間違っている場合 {
+        @DataPoints
+        public static Fixture[] FIXTURES = {
+                new Fixture("0120-03-99", false),
+                new Fixture("", false)
+        };
+
+
+        @Theory
+        public void checkTel_電話番号が間違っている場合はfalseが返却される(Fixture fixture) throws Exception {
+            boolean actual = certification.checkTel(fixture.value);
+            boolean expected = fixture.expected;
+
+            assertThat(actual, is(expected));
+        }
     }
 }
